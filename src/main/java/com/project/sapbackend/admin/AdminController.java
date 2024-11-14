3e5cd2eec2c42b3b7b2019cb35e669fec2fccdae
Rnbsov <rnbsov@gmail.com>
2024-11-14 20:42:20 +0500

    🎉 feat: Replace params retrieving logic with request body

--- src/main/java/com/project/sapbackend/admin/AdminController.java
+++ src/main/java/com/project/sapbackend/admin/AdminController.java
@@ -2,6 +2,7 @@ package com.project.sapbackend.admin;
 
 
 import com.project.sapbackend.auth.AuthenticationService;
+import com.project.sapbackend.dto.AddSupportGroupToIncidentRequestDto;
 import com.project.sapbackend.entity.IncidentRequest;
 import com.project.sapbackend.entity.User;
 import com.project.sapbackend.repository.UserRepository;
@@ -9,7 +10,6 @@ import com.project.sapbackend.service.IncidentRequestService;
 import com.project.sapbackend.service.SupportGroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,14 +29,10 @@ public class AdminController {
         return ResponseEntity.ok(userRepository.findAll());
     }
 
-    @PutMapping("/incident-request/{incidentId}")
-    public ResponseEntity<?> addSupportGroupToIncident(
-            @PathVariable Long incidentId,
-            @RequestParam Long groupId
-    ) {
-
-        IncidentRequest incidentRequest = incidentRequestService.findById(incidentId);
-        incidentRequest.setSupportGroup(supportGroupService.findById(groupId));
+    @PutMapping("/incident-request")
+    public ResponseEntity<?> addSupportGroupToIncident(@RequestBody AddSupportGroupToIncidentRequestDto request) {
+        IncidentRequest incidentRequest = incidentRequestService.findById(request.getIncidentId());
+        incidentRequest.setSupportGroup(supportGroupService.findById(request.getGroupId()));
         return ResponseEntity.ok(incidentRequestService.save(incidentRequest));
     }
 
